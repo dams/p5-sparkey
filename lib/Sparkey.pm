e30676b8e8b4db12c05d2e8f854cacf7bc593d46
Emmanuel 'BHS_error' Peroumalnaik <peroumalnaik.emmanuel@gmail.com>
2013-12-07 16:37:51 +0100

    Fixed broken tests

--- lib/Sparkey.pm
+++ lib/Sparkey.pm
@@ -5,6 +5,15 @@ use warnings;
 
 our $VERSION     = '0.001';
 
+use Exporter;
+use XSLoader;
+
+use base 'Exporter';
+
+# our %EXPORT_TAGS = ( 'all' => [qw<get_version>] );
+# our @EXPORT_OK   = ( @{ $EXPORT_TAGS{'all'} } );
+
+XSLoader::load(__PACKAGE__, $Sparkey::VERSION);
 
 1;
 
